6c3d616c978c58db0f1813df16591c5fe6792a7b
weiang <weiang@mail.ustc.edu.cn>
2012-10-25 16:18:02 +0800

    new file:   401.c

---
+++ 401.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 401.c
+	> Author: Weiang
+	> Mail: weiang@mail.ustc.edu.cn 
+	> Created Time: Wed 17 Oct 2012 12:06:33 AM CST
+    > Describition: UVA problems 401 "Palindromes"
+ ************************************************************************/
+
+#include <stdio.h>
+
+
+int palind(char *s, int n)
+{
+	int	i;
+
+	for (i = 0; i <= n / 2; i ++) {
+		if (s[i] ==
+int main(void)
+
